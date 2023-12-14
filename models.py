533c6f52501f698b0a5d01f03b17863cbe99f626
Roman-K26 <romankuspys777@gmail.com>
2023-12-14 14:04:19 +0300

    Commit 2

--- models.py
+++ models.py
@@ -9,11 +9,12 @@ class Owner(Base):
     fullname = Column(String(150))
     email = Column(String(150), unique=True, index=True)
 
-class Cats(Base):
+class Cat(Base):
     __tablename__ = "cats"
     id = Column(Integer, primary_key=True, index=True)
     nick = Column(String(50))
     age = Column(Integer)
+    vaccinated = Column(Boolean, default=False)
     owner_id =Column(Integer, ForeignKey("owners.id"), nullable=True)
     owner = relationship("Owner", backref="cats")
 
